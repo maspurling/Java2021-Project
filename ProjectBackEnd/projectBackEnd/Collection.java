e6a38cfbf41bccc4ae4a0396a1c5b97e89d2bff2
Makenzie Spurling <maklynnsp@gmail.com>
2021-02-15 14:06:11 -0600

    Created song & collection classes + filled out song class

---
+++ ProjectBackEnd/projectBackEnd/Collection.java
@@ -0,0 +1,16 @@
+package projectBackEnd;
+
+import java.io.BufferedReader;
+
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
+public class Collection {
+	
+}
---
+++ ProjectBackEnd/projectBackEnd/Song.java
@@ -0,0 +1,95 @@
+package projectBackEnd;
+
+public class Song {
+	private int id;
+	private String artist;
+	private String genre;
+	private String track;
+	private String album;
+	private int year;
+	private double longitude;
+	
+	// Default Constructor
+	public Song() {
+		id = 0;
+		artist = "not set";
+		genre = "not set";
+		track = "not set";
+		album = "not set";
+		year = 0;
+		longitude = 0;
+	}
+	
+	// Useful Constructor
+	public Song(int i, String a, String g, String t, String b, int y, double l) {
+		id = i;
+		artist = a;
+		genre = g;
+		track = t;
+		album = b;
+		year = y;
+		longitude = l;
+	}
+	
+	// Getters & Setters
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int i) {
+		id = i;
+	}
+	
+	public String getArtist() {
+		return artist;
+	}
+	
+	public void setArtist(String a) {
+		artist = a;
+	}
+	
+	public String getGenre() {
+		return genre;
+	}
+	
+	public void setGenre(String g) {
+		genre = g;
+	}
+	
+	public String getTrack() {
+		return track;
+	}
+	
+	public void setTrack(String t) {
+		track = t;
+	}
+	
+	public String getAlbum() {
+		return album;
+	}
+	
+	public void setAlbum(String a) {
+		album = a;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public void setYear(int y) {
+		year = y;
+	}
+	
+	public double getLong() {
+		return longitude;
+	}
+	
+	public void setLong(double l) {
+		longitude = l;
+	}
+
+	// toString because every class NEEDS a toString
+	public String toString() {
+		return id+" "+artist+" "+genre+" "+track+" "+album+" "+year+" "+longitude;
+	}
+}
